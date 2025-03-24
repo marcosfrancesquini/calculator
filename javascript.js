90fc80a3556f006a6797f1fd9ac87d42600a48a0
marcosfrancesquini <francesquini.personal@gmail.com>
2025-03-24 17:53:09 -0300

    Better Design

--- javascript.js
+++ javascript.js
@@ -1,4 +1,14 @@
+let total = 0;
+let greatTotal = 0;
+
 function clean(){
     document.getElementById('enter').value='';
     //Note: It is necessary also clear the variables' values
+}
+
+function pressPerCent(){
+    let value = document.getElementById('enter').value;
+    total = total + parseFloat(value);
+    document.getElementById('enter').value = total;
+    clean();
 }
