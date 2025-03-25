0ea62afd3ff89b66988c86af30038654654d88bb
marcosfrancesquini <francesquini.personal@gmail.com>
2025-03-25 17:41:03 -0300

    numbers 7 on display + logic

--- javascript.js
+++ javascript.js
@@ -2,13 +2,16 @@ let total = 0;
 let greatTotal = 0;
 
 function clean(){
-    document.getElementById('enter').value='';
-    //Note: It is necessary also clear the variables' values
+    document.getElementById('enter').value='0';    
+    let total = 0;
+    let greatTotal = 0;    
 }
 
 function pressPerCent(){
-    let value = document.getElementById('enter').value;
-    total = total + parseFloat(value);
-    document.getElementById('enter').value = total;
-    clean();
+    document.getElementById('enter').value="%";
+    //let value = document.getElementById('percentage').value;
+}
+
+function pressSeven(){
+    document.getElementById('enter').value="7";
 }
